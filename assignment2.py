0c577d1c75318af2c57f5ffa76bbafdfbbade7cb
Daniel Wheaton <dwheaton2011@gmail.com>
2014-09-10 15:38:10 -0500

    Completed homework. Learned to not name files after libraries. LOL.

--- assignment2.py
+++ assignment2.py
@@ -1,13 +1,10 @@
 #assignment number 3, using randomness 
+import random
 
-
-
-import random    
 names = ["Harvey Perlman","Bo Pelini","Dave Heineman","Jon Bruning", "Mike Foley", "John", "Deb Fischer","Lee Terry","Adrian Smith","Jeff Fortenberry"]
 
 verbs = ["cancelled school","lost","ignored a scandal","called plebians racoons","attacked his sister","lied","farmland","took dirty money","cursed","voted"]
     
-for verb in verbs:
 for name in names: 
-    print random.choice(names),"was very naughty, they" ,random.choice(verbs), "today." 
+    print name,"was very naughty, they", random.choice(verbs), "today." 
         
