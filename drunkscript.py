15fe2b1986b180442dad74ee99a2866e3c7edd3a
Daniel Wheaton <dwheaton2011@gmail.com>
2014-09-17 15:56:18 -0500

    Completed first story bot

--- drunkscript.py
+++ drunkscript.py
@@ -1,8 +1,25 @@
-import csv
+import csv,string
 
 reader = csv.reader(open("drunks.csv", "rU"),dialect=csv.excel)
 
 reader.next()
 
 for row in reader:
-    print row[0], "is a real drunk. They drank", row[1], " dranks in one sitting once." 
+    if int(row[1])>19: #all if statements have to have an else
+        exclamation = "Holy shit."
+        judgement = "real drunk"
+    elif int(row[1])<10: 
+        exclamation = "Teetotaler alert!"
+        judgement = "lightweight"
+    else: 
+        exclamation = "Merp."
+        judgement = ""
+    if row[0]=="Sara":
+        pronoun = "she"
+    else: 
+        pronoun = "he"
+        
+    record = 45
+    difference = record - int(row[1])
+    percent = (float(row[1])/float(record))*100
+    print "%s %s is a %s; %s drank %s drinks in one sitting once. That's %i off the record of %i or, %f.1 percent." % (exclamation, row[0], judgement, pronoun.title(), row[1], difference, record, percent)
---
+++ firstbot.py
@@ -0,0 +1,53 @@
+import csv, string, datetime
+
+reader = csv.reader(open("blsdata.csv", "rU"), dialect=csv.excel)
+
+reader.next()
+
+blslist = []
+
+for row in reader:
+    blslist.append(row)
+
+currentyear = blslist[-1]
+currentyear = filter(None,currentyear)
+
+yearlen = len(currentyear)
+
+if yearlen != 1:
+    currentmonth = currentyear[-1]
+    previousmonth = currentyear[-2]
+    reportyear = int(currentyear[0])
+    reportmonth = len(currentyear)-1
+    reportdate = datetime.datetime(reportyear, reportmonth, 1)
+else:
+    currentmonth = currentyear[1]
+    previousyear = blslist[-2]
+    previousmonth = previousyear[-1]
+    reportyear = int(currentyear[0])
+    reportmonth = len(currentyear)-1
+    reportdate = datetime.datetime(reportyear, reportmonth, 1)
+
+previousyear = blslist[-2]
+previousyearmonth = previousyear[yearlen-1]
+
+if currentmonth > previousmonth:
+    verb = "increased"
+    adjective = "higher"
+elif currentmonth == previousmonth:
+    verb = "held steady"
+    adjective = "the same as"
+else:
+    verb = "decreased"
+    adjective = "lower"
+    
+pctchange = ((float(currentmonth)-float(previousmonth))/float(previousmonth)*100)
+
+#current month, previous monht, previous years
+
+lastyear = (float(previousyearmonth)-float(currentmonth))
+
+
+
+print verb, abs(pctchange)
+print "WASHINGTON (AP) -- The United States' unemployment rate %s last month, to %s percent. That represents a percent change %s of percent from two months ago. That is %f percentage points %s than this time last year, according to the Bureau of Labor Statistics." % (verb, currentmonth, lastyear, abs(pctchange), adjective)
