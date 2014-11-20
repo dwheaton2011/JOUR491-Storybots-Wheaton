72536f266e8b3460db904a43c1f906eb9ee8299c
Daniel Wheaton <dwheaton2011@gmail.com>
2014-11-20 13:51:43 -0600

    Uploaded Waite Bot

---
+++ Waitebot/Waitebot.py
@@ -0,0 +1,69 @@
+from db import Db
+from gen import Generator
+from parse import Parser
+from sql import Sql
+from rnd import Rnd
+from twython import Twython
+import sys, random
+import sqlite3
+import codecs
+import time, string
+
+#import your own data
+#API_KEY = 
+#API_SECRET = 
+#ACCESS_TOKEN = 
+#ACCESS_TOKEN_SECRET = 
+
+twitter = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+
+SENTENCE_SEPARATOR = '.'
+WORD_SEPARATOR = ' '
+
+if __name__ == '__main__':
+    args = sys.argv
+    usage = 'Usage: %s (parse <name> <depth> <path to txt file>|gen <name> <count>)' % (args[0], )
+    
+    if (len(args) < 3):
+        raise ValueError(usage)
+    
+    mode  = args[1]
+    name  = args[2]
+    
+    if mode == 'parse':
+        if (len(args) != 5):
+            raise ValueError(usage)
+        
+        depth = int(args[3])
+        file_name = args[4]
+        
+        db = Db(sqlite3.connect(name + '.db'), Sql())
+        db.setup(depth)
+        
+        txt = codecs.open(file_name, 'r', 'utf-8').read()
+        Parser(name, db, SENTENCE_SEPARATOR, WORD_SEPARATOR).parse(txt)
+    
+    elif mode == 'gen':
+        count = int(args[3])
+        db = Db(sqlite3.connect(name + '.db'), Sql())
+        generator = Generator(name, db, Rnd())
+        for i in range(0, count):
+            markovs = generator.generate(WORD_SEPARATOR)
+            markovs = markovs.split("\n")
+            Waitetext = []
+            for mark in markovs:
+                Waitetext.append(mark)
+                tweets = []
+                for tw in Waitetext:
+                    if len(tw)<140 and len(tw)>3:
+                        tweets.append(tw)
+                    else:
+                        pass
+            try:
+                tweet=random.choice(tweets)
+                print tweet
+                twitter.update_status(status=tweet)
+                time.sleep(5)
+            except:
+                continue
